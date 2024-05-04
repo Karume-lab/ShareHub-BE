c5e17f29922a587cc32a3f928d687a9076a7eaa4
Karume-lab <danielkarume@outlook.com>
2024-05-04 21:28:46 +0300

    use custom user model

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,12 @@
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
+from core import serializers as core_ser
 from . import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        model = User
+        model = models.CustomUser
         fields = ("id", "email", "password")
 
 
