e023d0d1b8fa16e7915c5a3cbf58eafe9c405b58
Karume-lab <danielkarume@outlook.com>
2024-05-04 19:37:10 +0300

    add user profile serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,6 @@
 from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
+from . import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,3 +10,9 @@ class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ("id", "email", "password")
+
+
+class UserProfile(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = "__all__"
