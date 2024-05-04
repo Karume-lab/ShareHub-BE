56fe343f7d88f594657900e076200a4d745a9901
Karume-lab <danielkarume@outlook.com>
2024-05-04 21:28:34 +0300

    import user manager

--- accounts/models.py
+++ accounts/models.py
@@ -2,34 +2,13 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
-    BaseUserManager,
 )
-from django.contrib.auth.models import BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
+from . import managers
 from django.utils.translation import gettext_lazy as _
 
 
-class UserAccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        return self.create_user(email, password, **extra_fields)
-
-
-class CustomUserModel(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
@@ -39,7 +18,7 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_mod = models.BooleanField(default=False)
 
-    objects = UserAccountManager()
+    objects = managers.UserAccountManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ()
@@ -64,7 +43,7 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
-        CustomUserModel, verbose_name=_("User"), on_delete=models.CASCADE
+        CustomUser, verbose_name=_("User"), on_delete=models.CASCADE
     )
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=100)
@@ -85,3 +64,9 @@ class UserProfile(models.Model):
         if self.user:
             self.profile_picture.upload_to = f"profile_images/{self.user.email}"
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.email
+
+    def __repr__(self) -> str:
+        return self.user.email
