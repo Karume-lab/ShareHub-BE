216e00896eb9530c8648f8f9d4255f91be58da9a
Karume-lab <danielkarume@outlook.com>
2024-05-04 13:12:59 +0300

    configure cookies

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path, include
-
+from . import views
 
 urlpatterns = [
     path("", include("djoser.urls")),
-    path("", include("djoser.urls.jwt")),
+    path(
+        "jwt/create/",
+        view=views.CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "jwt/refresh/",
+        view=views.CustomTokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "jwt/verify/", view=views.CustomTokenVerifyView.as_view(), name="token_verify"
+    ),
+    path("logout/", view=views.LogoutView.as_view(), name="auth_logout"),
 ]
