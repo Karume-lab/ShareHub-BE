6c9907d85d63da6aa656bd46e2c18ad125a2ebad
Karume-lab <danielkarume@outlook.com>
2024-05-04 13:13:54 +0300

    render all fields in serializer

--- core/serializers.py
+++ core/serializers.py
@@ -5,14 +5,4 @@ from . import models
 class Innovation(serializers.ModelSerializer):
     class Meta:
         model = models.Innovation
-        fields = (
-            "author",
-            "title",
-            "description",
-            "dataset",
-            "created_at",
-            "updated_at",
-            "status",
-            "categories",
-            "slug",
-        )
+        fields = "__all__"
