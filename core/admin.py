8f7b61c649d5fa5eafb4b7691d3c33112bfa9057
Karume-lab <danielkarume@outlook.com>
2024-05-06 15:20:18 +0300

    add id for the innovations

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from . import models
 
 
 class InnovationAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "created_at", "updated_at", "status")
+    list_display = ("id", "title", "author", "created_at", "updated_at", "status")
 
 
 admin.site.register(models.Innovation, InnovationAdmin)
