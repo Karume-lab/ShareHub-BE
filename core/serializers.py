c608dfa5895bc20ab801dab25b88c6c3ddc3588b
Karume-lab <danielkarume@outlook.com>
2024-05-06 15:20:26 +0300

    fix innovation creation

--- core/serializers.py
+++ core/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
 from accounts import serializers as accounts_serializers
 from . import models
-1
+
 
 class Innovation(serializers.ModelSerializer):
-    author = accounts_serializers.Author(read_only=True)
-
     class Meta:
         model = models.Innovation
         fields = (
