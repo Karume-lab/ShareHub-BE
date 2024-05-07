a7e17f3693ea63771ebe3109224a23f86dc93dc7
Karume-lab <danielkarume@outlook.com>
2024-05-08 01:40:40 +0300

    register comment models in admin panel

--- core/admin.py
+++ core/admin.py
@@ -6,4 +6,9 @@ class InnovationAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "author", "created_at", "updated_at", "status")
 
 
+class InnovationCommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "text", "created_at", "updated_at", "likes")
+
+
 admin.site.register(models.Innovation, InnovationAdmin)
+admin.site.register(models.InnovationComment, InnovationCommentAdmin)
