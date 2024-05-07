e70991f7a20f74b5d67102bb8290db8e5770b6e5
Karume-lab <danielkarume@outlook.com>
2024-05-08 01:40:27 +0300

    add comments

--- core/models.py
+++ core/models.py
@@ -33,9 +33,35 @@ class Innovation(models.Model):
     category = models.CharField(
         _("Category"), max_length=1, default="H", choices=CATEGORY_CHOICES
     )
+    likes = models.IntegerField(_("Likes"))
 
     def __str__(self) -> str:
         return self.title
 
     def __repr__(self) -> str:
         return self.title
+
+
+class Forum(models.Model):
+    pass
+
+
+class BaseComment(models.Model):
+    author = models.ForeignKey("accounts.UserProfile", on_delete=models.CASCADE)
+    text = models.TextField(_("Comment"))
+    created_at = models.DateTimeField(_("Date created"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Date updated"), auto_now=True)
+    likes = models.IntegerField(_("Likes"))
+
+    class Meta:
+        abstract = True
+
+
+class InnovationComment(BaseComment):
+    innovation = models.ForeignKey(
+        "Innovation", on_delete=models.CASCADE, related_name="innovation_comments"
+    )
+
+
+class ForumComment(BaseComment):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
