b9c0fef1c1faea7529106cd4e07b33d22299277f
Karume-lab <danielkarume@outlook.com>
2024-05-08 10:14:26 +0300

    fix the creation of innovations view

--- core/views.py
+++ core/views.py
@@ -23,16 +23,14 @@ def innovation_list(request):
                 {"detail": "User not authenticated"},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
-        data = request.data.copy()
-        user_profile = accounts_models.UserProfile.objects.get(
-            pk=request.user.user_profile.pk
-        )
-        data["author"] = user_profile.pk
-        serializer = serializers.Innovation(data=data, context={"request": request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = serializers.Innovation(data=request.data, context={"request": request})
+
+    if serializer.is_valid():
+        serializer.validated_data["author"] = request.user.user_profile
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "PATCH", "DELETE"])
@@ -110,15 +108,96 @@ def innovation_comment_list(request, pk):
     List all comments for a specific innovation, or create a comment for an innovation
     """
     if request.method == "GET":
-        comments = models.InnovationComment.objects.filter(innovation_id=pk)
+        innovation_comments = models.InnovationComment.objects.filter(innovation_id=pk)
         serializer = serializers.InnovationComment(
-            comments, many=True, context={"request": request}
+            innovation_comments, many=True, context={"request": request}
         )
         return Response(serializer.data)
 
     elif request.method == "POST":
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "User not authenticated"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        request.data["innovation"] = pk
+        request.data["author"] = request.user.user_profile.pk
         serializer = serializers.InnovationComment(data=request.data)
         if serializer.is_valid():
-            serializer.save(pk=pk)
+            serializer.save(innovation_id=pk)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+def innovation_comment_detail(request, pk):
+    """
+    Contains methods for updating an innovation comment (either partially or entirely) and deleting an innovation comment
+    """
+    try:
+        innovation_comment = models.InnovationComment.objects.get(pk=pk)
+    except models.InnovationComment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = serializers.InnovationComment(
+            innovation_comment, context={"request": request}
+        )
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "User not authenticated"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        if request.user != innovation_comment.author.user:
+            return Response(
+                {"detail:" "Cannot update"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = serializers.InnovationComment(
+            innovation_comment, data=request.data, context={"request": request}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "PATCH":
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "User not authenticated"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        if request.user != innovation_comment.author.user:
+            return Response(
+                {"detail": "Cannot update"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = serializers.InnovationComment(
+            innovation_comment,
+            data=request.data,
+            partial=True,
+            context={"request": request},
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "User not authenticated"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        if request.user != innovation_comment.author.user:
+            return Response(
+                {"detail:" "Cannot delete"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        innovation_comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
