ff4a741d6478461b500f2b5fd5d64b6557fd8736
Karume-lab <danielkarume@outlook.com>
2024-05-08 10:53:44 +0300

    register the detail view for innovation comments

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 
 urlpatterns = [
+    path(
+        "innovation_comments/<int:pk>/",
+        views.innovation_comment_detail,
+        name="innovationcomment-detail",
+    ),
     path("innovations/", views.innovation_list, name="innovation-list"),
     path("innovations/<int:pk>/", views.innovation_detail, name="innovation-detail"),
     path(
         "innovations/<int:pk>/comments/",
         views.innovation_comment_list,
         name="innovationcomment-list",
-    ),
-    path(
-        "comments/<int:pk>/",
-        views.innovation_comment_detail,
-        name="innovationcomment-detail",
     ),
 ]
