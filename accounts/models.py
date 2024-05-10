1fdd76f0c22ec681bb342ecb035676af4a864947
Karume-lab <danielkarume@outlook.com>
2024-05-10 11:52:10 +0300

    add roles

--- accounts/models.py
+++ accounts/models.py
@@ -14,9 +14,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = "Users"
 
     email = models.EmailField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_mod = models.BooleanField(default=False)
+    is_site_mod = models.BooleanField(default=False)
+    is_forum_mod = models.BooleanField(default=False)
 
     objects = managers.UserAccountManager()
 
