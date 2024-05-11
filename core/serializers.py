83f46d78d9ba5cdf48b51d3e99446ea6ed10f940
Karume-lab <danielkarume@outlook.com>
2024-05-11 06:04:17 +0300

    add is_liked

--- core/serializers.py
+++ core/serializers.py
@@ -5,6 +5,7 @@ from . import models
 
 class Innovation(serializers.ModelSerializer):
     author = accounts_serializers.Author(read_only=True)
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Innovation
@@ -17,8 +18,31 @@ class Innovation(serializers.ModelSerializer):
             "dashboard_image",
             "created_at",
             "updated_at",
+            "is_liked",
             "status",
             "category",
+        )
+
+    def get_is_liked(self, obj):
+        try:
+            author = self.context.get("request").user.user_profile
+            is_liked = obj.get_is_liked(author)
+        except AttributeError:
+            is_liked = False
+        return is_liked
+
+
+class Like(serializers.ModelSerializer):
+    class Meta:
+        model = models.Like
+        fields = (
+            "url",
+            "author",
+            "innovation",
+        )
+        read_only_fields = (
+            "author",
+            "innovation",
         )
 
 
@@ -36,5 +60,4 @@ class InnovationComment(serializers.ModelSerializer):
             "text",
             "created_at",
             "updated_at",
-            "likes",
         )
