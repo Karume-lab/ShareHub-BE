796ecc747f5448f71405eb0503c2969771282347
Karume-lab <danielkarume@outlook.com>
2024-05-12 11:32:35 +0300

    update allowed hosts

--- sharehub/settings.py
+++ sharehub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-d&bmbqt^mo2xzhsnhce1&a8vq6bh7hp%4r1z$#o*#)17#z(-=b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
